126db79c3fb260b40eef6e20004d767af91fe2d3
Harsh <harshjindal955@gmail.com>
2026-01-24 00:33:28 +0530

    add the mongoose relation validation and LinkSchema

--- src/model/content.ts
+++ src/model/content.ts
@@ -10,8 +10,34 @@ const ContentSchema = new Schema({
 	link: String,
 	tags: [{ type: mongoose.Types.ObjectId, ref: "Tag" }],
 	type: String,
-	userId: { type: mongoose.Types.ObjectId, ref: "user", required: true },
+	userId: {
+		type: mongoose.Types.ObjectId,
+		ref: "user",
+		required: true,
+		validate: {
+			// We use an async validator
+			validator: async function (
+				value: mongoose.Types.ObjectId,
+			): Promise<boolean> {
+				// Use .exists() for better performance as it returns only the _id or null
+				const user = await model("User").exists({ _id: value });
+				return !!user;
+			},
+			message: "The provided userId does not exist in the database.",
+		},
+	},
+});
+
+const LinkSchema = new Schema({
+	hash: String,
+	userId: {
+		type: mongoose.Types.ObjectId,
+		ref: "user",
+		required: true,
+		unique: true,
+	},
 });
 
 export const UserModel = mongoose.model("user", UserSchema);
 export const ContentModel = mongoose.model("content", ContentSchema);
+export const LinkModel = mongoose.model("link", LinkSchema);
